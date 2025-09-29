8aac94c0f014366f4dde379caf82cdb07ce28884
Cameron Ross <elpenguino@gmail.com>
2025-09-29 01:59:10 -0300

    m4a: add modules to package

--- m4a/source/retroseq/m4a/package.d
+++ m4a/source/retroseq/m4a/package.d
@@ -1,2 +1,11 @@
 ///
 module retroseq.m4a;
+
+public import retroseq.m4a.cgb_audio;
+public import retroseq.m4a.cgb_tables;
+public import retroseq.m4a.internal;
+public import retroseq.m4a.m4a;
+public import retroseq.m4a.m4a_tables;
+public import retroseq.m4a.mp2k_common;
+public import retroseq.m4a.music_player;
+public import retroseq.m4a.sound_mixer;
